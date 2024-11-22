1f72710e7a95beef8b28887c88a72a17859a2ab4
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2024-11-22 14:31:40 +0000

    Add types in export

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
 
@@ -24,7 +24,10 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
     terser(),
+    typescript({
+      rollupCommonJSResolveHack: false,
+      clean: true,
+    }),
   ],
 };
